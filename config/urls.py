5d7d1545dd61dbc1ea8cb29e5e207d182770b061
Elmurod-Zulfiqarov <77602143+Elmurod-Zulfiqarov@users.noreply.github.com>
2022-01-24 18:13:15 +0500

    Add Swagger OpenApi Docs

--- config/urls.py
+++ config/urls.py
@@ -1,20 +1,19 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Farm Application Rest API",
+        default_version="v1",
+        description="Swagger docs for Rest API",
+        contact=openapi.Contact(email="elmurodzulfiqarov@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(AllowAny, )
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +21,5 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-docs")
 ]
