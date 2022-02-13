4bad3b567e089dd1e25254fc7180a5bbe3bcbcd9
Elmurod-Zulfiqarov <77602143+Elmurod-Zulfiqarov@users.noreply.github.com>
2022-02-13 16:06:24 +0500

    dd
    ss

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Accounts(User):
     definition = models.TextField(max_length=4096, null=True, blank=True)
     salary = models.CharField(max_length=255, null=True, blank=True)
-    image = models.FileField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     passport = models.FileField(null=True, blank=True)
     phone = models.CharField(max_length=13, null=True, blank=True)
     phone_farm = models.CharField(max_length=13, null=True, blank=True)
